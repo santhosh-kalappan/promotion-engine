b5e81d1fa7e4c37eed42b9f4a95de3a265f83abf
santhosh-kalappan <53265110+santhosh-kalappan@users.noreply.github.com>
2021-05-02 06:31:53 +0100

    springboot project creation

---
+++ src/test/java/org/coding/challenge/promotionengine/PromotionEngineApplicationTests.java
@@ -0,0 +1,13 @@
+package org.coding.challenge.promotionengine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PromotionEngineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
